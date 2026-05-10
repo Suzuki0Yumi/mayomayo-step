716593c5258ec491465121940e69534cf7c205cf
Suzuki Yumi <noi6ry@gmail.com>
2026-05-10 18:52:01 +0900

    JSの呼び込み修正追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -55,3 +55,8 @@ const initStepForm = () => {
 document.addEventListener("DOMContentLoaded", initStepForm);
 document.addEventListener("turbo:load", initStepForm);
 
+// ④ デモ機能の読み込み
+import "./lp_demo"
+
+// ⑤ ヘッダーメニューの読み込み
+import "./header"
