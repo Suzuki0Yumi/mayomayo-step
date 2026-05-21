3a9f3f3254f6b4a9c15ad67953dcb15c2020a8bd
Suzuki Yumi <noi6ry@gmail.com>
2026-05-21 16:59:21 +0900

    userモデルとコントローラにコールバック処理と認証用メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,12 @@ class User < ApplicationRecord
     today_proposals_count >= DAILY_PROPOSAL_LIMIT
   end
 
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      # user.name = auth.info.name
+      # user.image = auth.info.image
+    end
+  end
 end
